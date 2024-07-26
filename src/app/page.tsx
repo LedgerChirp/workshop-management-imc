706d525d9a884c81e68c9802eee59a356da4c588
kanishk6103 <mamta11tiwari06@gmail.com>
2024-07-26 20:06:25 +0530

    added figma link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
         {/* navbar */}
         {/* dashboard */}
         {/* footer */}
+        {/* https://www.figma.com/design/YhdjTPHt3kQLTQsJ6xLu9K/Inventory-Management-System-(Community)?node-id=55-100475&t=7sv6RnPTeXgTBTCc-0 */}
       </div>
     </main>
   );
